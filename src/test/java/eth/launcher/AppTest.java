fb7ad0f2dc8e5bf1d922408dfe1838e85c7291ec
Karim TAAM <karim.t2am@gmail.com>
2020-10-20 16:51:47 +0200

    fix test and add sonar

--- src/test/java/eth/launcher/AppTest.java
+++ src/test/java/eth/launcher/AppTest.java
@@ -8,9 +8,6 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class AppTest {
-  @Test
-  public void testAppHasAGreeting() {
-    App classUnderTest = new App();
-    assertNotNull("app should have a greeting", classUnderTest.getGreeting());
-  }
+
+
 }
